a263f159f060b0764ea113cf67a6331a3abf6f3e
Elena Khaurova <radiofobia@list.ru>
2019-03-20 16:53:44 +0000

    minor

--- src/tests/ArticleTests.java
+++ src/tests/ArticleTests.java
@@ -2,8 +2,6 @@ package tests;
 
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
-import lib.ui.MyListsPageObject;
-import lib.ui.NavigationUI;
 import lib.ui.SearchPageObject;
 import org.junit.Assert;
 import org.junit.Test;
